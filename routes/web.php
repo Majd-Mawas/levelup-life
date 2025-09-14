60841c4ca055fdf73f691ae32d52f4002b9bba6d
Majd Mawas <majosmws@gmail.com>
2025-09-15 00:56:39 +0300

    add initial

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+require __DIR__ . '/auth.php';
+
+Route::group(['prefix' => '/', 'middleware' => 'auth'], function () {
+    Route::get('', [RoutingController::class, 'index'])->name('root');
+    Route::get('/home', fn() => view('index'))->name('home');
+    Route::get('{first}/{second}/{third}', [RoutingController::class, 'thirdLevel'])->name('third');
+    Route::get('{first}/{second}', [RoutingController::class, 'secondLevel'])->name('second');
+    Route::get('{any}', [RoutingController::class, 'root'])->name('any');
+});
